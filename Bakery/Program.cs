eb8f51f8ed1015ae03ae6feb87fb46e06d628227
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-15 13:07:07 -0700

    add writelines explaining bakery prices.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,7 +8,11 @@ namespace Bakery.Models
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Welcome to the Cafe");
+      Console.WriteLine("A loaf of bread costs $" + Bread.Price);
+      Console.WriteLine("Every third loaf of bread is free, so make sure you keep that in mind when you order!");
+      Console.WriteLine("A pastry is $" + Pastry.Price);
+      Console.WriteLine("If you buy two pastries then the third is 50% off");
     }
   }
 }
