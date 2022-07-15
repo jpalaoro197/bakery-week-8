e6df52ce1f1911033745b49639d49531685f6774
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-15 09:10:31 -0700

    add boiler plate code.

---
+++ Cafe.Tests/Model.Tests/BakeryTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+    [TestClass]
+    public class Bread Tests
+    {
+    
+    }
+
+    [TestClass]
+    public class Pastry Tests
+    {
+    
+    }
+
+}
