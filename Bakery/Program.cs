a28d38ffee6e5c59ed25425ce23087f60d9e8104
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-15 12:56:44 -0700

    add basic structure.

---
+++ Bakery/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Bakery.Models;
+
+namespace Bakery.Models
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
