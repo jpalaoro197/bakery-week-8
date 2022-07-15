9b640d8f94167531ae9e2b7db0c19f50217d75a7
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-15 13:26:46 -0700

    add writeline asking user how many loafs they want, and a readline parsing the number passed through.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,11 +8,13 @@ namespace Bakery.Models
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to the Cafe");
+      Console.WriteLine("Welcome to the Bakery");
       Console.WriteLine("A loaf of bread costs $" + Bread.Price);
       Console.WriteLine("Every third loaf of bread is free, so make sure you keep that in mind when you order!");
       Console.WriteLine("A pastry is $" + Pastry.Price);
       Console.WriteLine("If you buy two pastries then the third is 50% off");
+      Console.WriteLine("How many loafs of bread would you like?");
+      int breadResponse = int.parse(Console.ReadLine());
     }
   }
 }
